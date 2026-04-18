35fc9e84bbeb05c5abc0566bec6336a2408927d2
pallavi15x <pallavikumari15102007@gmail.com>
2026-04-18 11:23:55 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
     <nav className="flex justify-between items-center p-6 bg-white shadow-md w-full z-50 relative">
       <div className="font-bold text-xl text-orange-600">PARUL UNIVERSITY</div>
       <div className="space-x-8 hidden md:flex font-medium text-gray-700">
-        <Link to="/" className="hover:text-orange-500">Home</Link>
-        <Link to="/about" className="hover:text-orange-500">About</Link>
-        <Link to="/events" className="hover:text-orange-500">Events</Link>
+        <Link to="/" className="hover:text-orange-500 transition">Home</Link>
+        <Link to="/about" className="hover:text-orange-500 tarnsition">About</Link>
+        <Link to="/events" className="hover:text-orange-500 transition">Events</Link>
       </div>
-      <button className="bg-yellow-400 px-6 py-2 rounded-full font-bold text-black">
+      <button className="bg-yellow-400 px-6 py-2 rounded-full font-bold text-black transition">
         Apply Now 2026
       </button>
     </nav>
